2328e6443e1a48431c6d48b4ff82a96d6db13708
Alex Nweke <nwekealexchinedu@gmail.com>
2020-12-01 18:42:59 +0100

    created some text input fields from react native paper

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import CreateEmployee from './screens/CreateEmployee';
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			{/* <Text>Hello Bro Chima, I'm testing expo</Text> */}
-			<Home />
+			{/* <Home /> */}
+			<CreateEmployee />
 			<StatusBar style="auto" />
 		</View>
 	);
--- screens/CreateEmployee.jsx
+++ screens/CreateEmployee.jsx
@@ -8,14 +8,54 @@ const CreateEmployee = () => {
 	const [ modal, setModal ] = React.useState(false);
 	return (
 		<View style={styles.root}>
-			<Text>hello from the CreateEmployee component</Text>
+			<TextInput
+				label="Name"
+				value={formValues.name}
+				style={styles.inputStyle}
+				mode="outlined"
+				theme={theme}
+				onChangeText={(text) => setFormValues({ ...formValues, name: text })}
+			/>
+			<TextInput
+				label="Email"
+				value={formValues.email}
+				style={styles.inputStyle}
+				mode="outlined"
+				theme={theme}
+				onChangeText={(text) => setFormValues({ ...formValues, email: text })}
+			/>
+			<TextInput
+				label="Phone Number"
+				value={formValues.phoneNumber}
+				style={styles.inputStyle}
+				mode="outlined"
+				theme={theme}
+				onChangeText={(text) => setFormValues({ ...formValues, phoneNumber: text })}
+			/>
+			<TextInput
+				label="Salary"
+				value={formValues.salary}
+				style={styles.inputStyle}
+				mode="outlined"
+				theme={theme}
+				onChangeText={(text) => setFormValues({ ...formValues, salary: text })}
+			/>
 		</View>
 	);
+};
+
+const theme = {
+	colors: {
+		primary: '#006aff'
+	}
 };
 
 const styles = StyleSheet.create({
 	root: {
 		flex: 1
+	},
+	inputStyle: {
+		margin: 5
 	}
 });
 
