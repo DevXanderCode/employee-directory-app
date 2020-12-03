f7f2dbef4c602a241acd40e099fef64c806e7ba9
Alex Nweke <nwekealexchinedu@gmail.com>
2020-12-03 09:47:42 +0100

    added the profile image to the profile component and center align it

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const Profile = () => {
 	return (
 		<View style={styles.root}>
 			<LinearGradient colors={[ '#0033ff', '#6bc1ff' ]} style={{ height: '20%' }} />
+			<View
+				style={{
+					// flexDirection: 'row',
+					alignItems: 'center',
+					position: 'relative',
+					bottom: 70
+				}}
+			>
+				<Image
+					source={{
+						uri:
+							'https://images.unsplash.com/photo-1579503841516-e0bd7fca5faa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjN8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+					}}
+					style={{
+						width: 140,
+						height: 140,
+						borderRadius: 70
+					}}
+				/>
+			</View>
 			<Text>Hello from the profile component</Text>
 		</View>
 	);
