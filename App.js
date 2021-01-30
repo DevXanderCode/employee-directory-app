4c4cdafb2f30674acadc13b1200f65b91ba3ca1b
Alex Nweke <nwekealexchinedu@gmail.com>
2021-01-30 12:56:23 +0100

    tried uploading image to cloudinary

--- App.js
+++ App.js
@@ -1,52 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Constants from 'expo-constants';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/Home';
-import CreateEmployee from './screens/CreateEmployee';
-import Profile from './screens/Profile';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import CreateEmployee from "./screens/CreateEmployee";
+import Profile from "./screens/Profile";
 
 const Stack = createStackNavigator();
 
 const myOptions = {
-	title: 'Home Screen',
-	headerTintColor: '#fff',
-	headerStyle: {
-		backgroundColor: '#006aff'
-	}
+  title: "Home Screen",
+  headerTintColor: "#fff",
+  headerStyle: {
+    backgroundColor: "#006aff",
+  },
 };
 
 function App(props) {
-	return (
-		<View style={styles.container}>
-			<Stack.Navigator>
-				<Stack.Screen name="Home" component={Home} options={myOptions} />
-				<Stack.Screen name="Profile" component={Profile} options={{ ...myOptions, title: 'Profile' }} />
-				<Stack.Screen
-					name="Create"
-					component={CreateEmployee}
-					options={{ ...myOptions, title: 'Create Employee' }}
-				/>
-			</Stack.Navigator>
-			<StatusBar style="auto" />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={myOptions} />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ ...myOptions, title: "Profile" }}
+        />
+        <Stack.Screen
+          name="Create"
+          component={CreateEmployee}
+          options={{ ...myOptions, title: "Create Employee" }}
+        />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 export default () => {
-	return (
-		<NavigationContainer>
-			<App />
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#ebebeb'
-		// marginTop: Constants.statusBarHeight
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#ebebeb",
+    // marginTop: Constants.statusBarHeight
+  },
 });
--- screens/CreateEmployee.jsx
+++ screens/CreateEmployee.jsx
@@ -1,147 +1,217 @@
-import * as React from 'react';
-import { StyleSheet, View, Text, Modal } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
-import * as ImagePicker from 'expo-image-picker';
+import * as React from "react";
+import { StyleSheet, View, Text, Modal } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 // import Constants from "expo-constants";
-import * as Permissions from 'expo-permissions';
+import * as Permissions from "expo-permissions";
 
 const CreateEmployee = () => {
-	const [ formValues, setFormValues ] = React.useState({ name: '', phoneNumber: '', email: '', salary: '' });
-	const [ picture, setPicture ] = React.useState('');
-	const [ modalVisible, setModalVisible ] = React.useState(false);
+  const [formValues, setFormValues] = React.useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    salary: "",
+  });
+  const [picture, setPicture] = React.useState("");
+  const [modalVisible, setModalVisible] = React.useState(false);
 
-	const pickImageFromGallery = async () => {
-		const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-		if (granted) {
-			let data = await ImagePicker.launchImageLibraryAsync({
-				mediaTypes: ImagePicker.MediaTypeOptions.Images,
-				allowsEditing: true,
-				aspect: [ 1, 1 ],
-				quality: 0.5
-			});
-			console.log('Logging Data', data);
-		} else {
-			alert('Sorry, we need camera roll permissions to make this work!');
-		}
-	};
+  const pickImageFromGallery = async () => {
+    // const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (status === "granted") {
+      let data = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 0.5,
+      });
+      console.log(
+        "Logging Data after change the way i asked for permission",
+        data
+      );
+      if (!data.cancelled) {
+        let splitStr = data.uri.split(".");
+        let newFile = {
+          uri: data.uri,
+          type: `${splitStr[splitStr.length - 1]}`,
+          name: `${data.uri.split("ImagePicker/")[1]}`,
+        };
+        console.log("Logging new File", newFile);
+        handleUpload(newFile);
+      }
+    } else {
+      alert("Sorry, we need camera roll permissions to make this work!");
+    }
+  };
 
-	const pickImageFromCamera = async () => {
-		const { granted } = await Permissions.askAsync(Permissions.CAMERA);
-		if (granted) {
-			let data = await ImagePicker.launchCameraAsync({
-				mediaTypes: ImagePicker.MediaTypeOptions.Images,
-				allowsEditing: true,
-				aspect: [ 1, 1 ],
-				quality: 0.5
-			});
-		} else {
-			alert('Sorry, we need camera roll permissions to make this work!');
-		}
-	};
+  const pickImageFromCamera = async () => {
+    // const { granted } = await Permissions.askAsync(Permissions.CAMERA);
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      let data = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 0.5,
+      });
+      console.log(
+        "logging data of the camera roll after changing the way i asked for permission ",
+        data
+      );
+      if (!data.cancelled) {
+        let splitStr = data.uri.split(".");
+        let newFile = {
+          uri: data.uri,
+          type: `${splitStr[splitStr.length - 1]}`,
+          name: `${data.uri.split("ImagePicker/")[1]}`,
+        };
 
-	return (
-		<View style={styles.root}>
-			<TextInput
-				label="Name"
-				value={formValues.name}
-				style={styles.inputStyle}
-				mode="outlined"
-				theme={theme}
-				onChangeText={(text) => setFormValues({ ...formValues, name: text })}
-			/>
-			<TextInput
-				label="Email"
-				value={formValues.email}
-				style={styles.inputStyle}
-				mode="outlined"
-				theme={theme}
-				onChangeText={(text) => setFormValues({ ...formValues, email: text })}
-			/>
-			<TextInput
-				label="Phone Number"
-				value={formValues.phoneNumber}
-				style={styles.inputStyle}
-				mode="outlined"
-				keyboardType="number-pad"
-				theme={theme}
-				onChangeText={(text) => setFormValues({ ...formValues, phoneNumber: text })}
-			/>
-			<TextInput
-				label="Salary"
-				value={formValues.salary}
-				style={styles.inputStyle}
-				mode="outlined"
-				theme={theme}
-				onChangeText={(text) => setFormValues({ ...formValues, salary: text })}
-			/>
-			<Button
-				mode="contained"
-				style={styles.inputStyle}
-				theme={theme}
-				icon="upload"
-				onPress={() => setModalVisible(true)}
-			>
-				Upload image
-			</Button>
-			<Button
-				mode="contained"
-				style={styles.inputStyle}
-				theme={theme}
-				icon="content-save"
-				onPress={() => console.log('Saving')}
-			>
-				Save
-			</Button>
-			<Modal
-				animationType="slide"
-				transparent={true}
-				visible={modalVisible}
-				onRequestClose={() => {
-					setModalVisible(false);
-				}}
-			>
-				<View style={styles.modalView}>
-					<View style={styles.modalButtonView}>
-						<Button mode="contained" icon="camera" theme={theme} onPress={() => pickImageFromCamera()}>
-							Camera
-						</Button>
-						<Button mode="contained" theme={theme} icon="image" onPress={() => pickImageFromGallery()}>
-							Gallary
-						</Button>
-					</View>
-					<Button theme={theme} onPress={() => setModalVisible(false)}>
-						Cancel
-					</Button>
-				</View>
-			</Modal>
-		</View>
-	);
+        handleUpload(newFile);
+      }
+    } else {
+      alert("Sorry, we need camera roll permissions to make this work!");
+    }
+  };
+
+  const handleUpload = (image) => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "employeeApp");
+    data.append("cloud_name", "chinedu");
+
+    fetch("https://api.cloudinary.com/v1_1/chinedu/image/upload", {
+      method: "POST",
+      body: data,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("logging data in then", data))
+      .catch((err) =>
+        console.log(
+          "I got this error when i tried to upload to cloudinary",
+          err
+        )
+      );
+  };
+
+  return (
+    <View style={styles.root}>
+      <TextInput
+        label="Name"
+        value={formValues.name}
+        style={styles.inputStyle}
+        mode="outlined"
+        theme={theme}
+        onChangeText={(text) => setFormValues({ ...formValues, name: text })}
+      />
+      <TextInput
+        label="Email"
+        value={formValues.email}
+        style={styles.inputStyle}
+        mode="outlined"
+        theme={theme}
+        onChangeText={(text) => setFormValues({ ...formValues, email: text })}
+      />
+      <TextInput
+        label="Phone Number"
+        value={formValues.phoneNumber}
+        style={styles.inputStyle}
+        mode="outlined"
+        keyboardType="number-pad"
+        theme={theme}
+        onChangeText={(text) =>
+          setFormValues({ ...formValues, phoneNumber: text })
+        }
+      />
+      <TextInput
+        label="Salary"
+        value={formValues.salary}
+        style={styles.inputStyle}
+        mode="outlined"
+        theme={theme}
+        onChangeText={(text) => setFormValues({ ...formValues, salary: text })}
+      />
+      <Button
+        mode="contained"
+        style={styles.inputStyle}
+        theme={theme}
+        icon="upload"
+        onPress={() => setModalVisible(true)}
+      >
+        Upload image
+      </Button>
+      <Button
+        mode="contained"
+        style={styles.inputStyle}
+        theme={theme}
+        icon="content-save"
+        onPress={() => console.log("Saving")}
+      >
+        Save
+      </Button>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.modalView}>
+          <View style={styles.modalButtonView}>
+            <Button
+              mode="contained"
+              icon="camera"
+              theme={theme}
+              onPress={() => pickImageFromCamera()}
+            >
+              Camera
+            </Button>
+            <Button
+              mode="contained"
+              theme={theme}
+              icon="image"
+              onPress={() => pickImageFromGallery()}
+            >
+              Gallary
+            </Button>
+          </View>
+          <Button theme={theme} onPress={() => setModalVisible(false)}>
+            Cancel
+          </Button>
+        </View>
+      </Modal>
+    </View>
+  );
 };
 
 const theme = {
-	colors: {
-		primary: '#006aff'
-	}
+  colors: {
+    primary: "#006aff",
+  },
 };
 
 const styles = StyleSheet.create({
-	root: {
-		flex: 1
-	},
-	inputStyle: {
-		margin: 5
-	},
-	modalView: {
-		position: 'absolute',
-		bottom: 2,
-		width: '100%',
-		backgroundColor: '#fff'
-	},
-	modalButtonView: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		padding: 10
-	}
+  root: {
+    flex: 1,
+  },
+  inputStyle: {
+    margin: 5,
+  },
+  modalView: {
+    position: "absolute",
+    bottom: 2,
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  modalButtonView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
 });
 
 export default CreateEmployee;
