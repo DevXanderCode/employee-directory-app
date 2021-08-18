50c8b64d09ffce6439159d5ccc93524d8cac1fc7
Alex Nweke <nwekealexchinedu@gmail.com>
2021-08-18 16:13:13 +0100

    wrote code to connect to mongodb database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('./Employee');
+
+const Employee = mongoose.model('employee');
+
+const mongoUri = '';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connection', () => {
+  console.log('Connected to mongo database');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Got this error when i tried to connect to the database', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to node js');
