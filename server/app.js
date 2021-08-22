c9c455d66428d3ad5e3d232b1d76c22a8c4292a1
Alex Nweke <nwekealexchinedu@gmail.com>
2021-08-22 11:09:46 +0100

    started working on the post route

--- server/app.js
+++ server/app.js
@@ -4,16 +4,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./Employee');
 
+app.use(express.json());
+
 const Employee = mongoose.model('employee');
 
-const mongoUri = '';
+const mongoUri =
+  'mongodb+srv://admin:0123456789@cluster0.mutb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connection', () => {
+mongoose.connection.on('connected', () => {
   console.log('Connected to mongo database');
 });
 
@@ -23,6 +26,11 @@ mongoose.connection.on('error', (err) => {
 
 app.get('/', (req, res) => {
   res.send('Welcome to node js');
+});
+
+app.post('/send-data', (req, res) => {
+  console.log(req.body);
+  res.send('posted');
 });
 
 app.listen(8080, () => {
